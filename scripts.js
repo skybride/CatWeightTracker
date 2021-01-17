6f62763d5e55ed48d4def7bb21ccfa0d325867a7
sky <skybride@hotmail.com>
2021-01-17 11:29:54 -0500

    update to ES6 syntax

--- scripts.js
+++ scripts.js
@@ -1,73 +1,46 @@
-function updateDisplay(){
-    var weightNala = document.createElement("p");
-    var weightKiki = document.createElement("p");
-    var weightPekoe = document.createElement("p");
-    var weightRogue = document.createElement("p");
+/*Display input values into Data section*/
+updateDisplay = () => {
+    /* create a new p each time*/
+    const weightNala = document.createElement("p");
+    const weightKiki = document.createElement("p");
+    const weightPekoe = document.createElement("p");
+    const weightRogue = document.createElement("p");
 
-    var inputValue = document.getElementById("nalaWeight").value;
-    console.log(inputValue);
+    /*
+    - take userinput and store in variable
+    - clear input element with id nalaWeight
+    */
+    const inputValue = document.getElementById("nalaWeight").value;
     document.getElementById("nalaWeight").value = "";
 
-    var inputValue2 = document.getElementById("kikiWeight").value;
-    console.log(inputValue2);
+    const inputValue2 = document.getElementById("kikiWeight").value;
     document.getElementById("kikiWeight").value = "";
 
-    var inputValue3 = document.getElementById("pekoeWeight").value;
-    console.log(inputValue3);
+    const inputValue3 = document.getElementById("pekoeWeight").value;
     document.getElementById("pekoeWeight").value = "";
 
-    var inputValue4 = document.getElementById("rogueWeight").value;
-    console.log(inputValue4);
+    const inputValue4 = document.getElementById("rogueWeight").value;
     document.getElementById("rogueWeight").value = "";
 
-    var weightValueNala = document.createTextNode(inputValue);
-    var weightValueKiki = document.createTextNode(inputValue2);
-    var weightValuePekoe = document.createTextNode(inputValue3);
-    var weightValueRogue = document.createTextNode(inputValue4);
+    /*create nodes that will hold the variables above*/
+    const weightValueNala = document.createTextNode(inputValue);
+    const weightValueKiki = document.createTextNode(inputValue2);
+    const weightValuePekoe = document.createTextNode(inputValue3);
+    const weightValueRogue = document.createTextNode(inputValue4);
 
+    /*append nodes to p elements create at very top*/
     weightNala.appendChild(weightValueNala);
     weightKiki.appendChild(weightValueKiki);
     weightPekoe.appendChild(weightValuePekoe);
     weightRogue.appendChild(weightValueRogue);
 
-
+    /*display at specified element ids*/
     document.getElementById("nalaWeightDisplay").appendChild(weightNala);
     document.getElementById("kikiWeightDisplay").appendChild(weightKiki);
     document.getElementById("pekoeWeightDisplay").appendChild(weightPekoe);
     document.getElementById("rogueWeightDisplay").appendChild(weightRogue);
 
-    // if(validateNumber(inputValue) === true){
-    //     document.getElementById("nalaWeightDisplay").appendChild(weightNala);
-    // }
-    // else if(validateNumber(inputValue2) === true){
-    //     document.getElementById("kikiWeightDisplay").appendChild(weightKiki);
-    // }
-
-    // else if(validateNumber(inputValue3) === true){
-    //     document.getElementById("pekoeWeightDisplay").appendChild(weightPekoe);
-    // }
-
-    // else if(validateNumber(inputValue4) === true){
-    //     document.getElementById("rogueWeightDisplay").appendChild(weightRogue);
-    // }
-   
 }
 
-function validateNumber(weight){
-    var errorDisplay="";
-    var errorText= "";
+/*Store into local storage*/
 
-    var weightInt = parseInt(weight);
-    if(isNaN(weightInt)){
-
-        errorDisplay = document.createElement("p");
-        errorText = document.createTextNode("That was not a number, try again.");
-        errorDisplay.appendChild(errorText);
-
-        document.getElementById("errorDisplay").appendChild(errorDisplay);
-        return false;
-    } else {
-        console.log(weightInt);
-        return true;
-    }
-}
